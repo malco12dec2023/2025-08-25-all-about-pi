be8c605f26a3e65a1ac474e30d7893a5491989ef
malco12dec2023 <malco12dec2023@gmail.com>
2025-08-25 13:07:36 +0800

    Update not-really-the-start.py
    eng-grey-dyents. tehe.

--- not-really-the-start.py
+++ not-really-the-start.py
@@ -2,3 +2,39 @@
 """
 Fond of using py extension instead of txt. Tehe.
 """
+
+#in·gre·di·ent
+"""
+/inˈɡrēdēənt,iNGˈɡrēdēənt/
+noun: ingredient
+plural noun: ingredients
+> any of the foods or substances that are combined to make a particular dish.
+> a component part or element of something.
+"""
+
+#summaryListOfIngredients
+"""
+1> Raspberry Pi SBC with Wifi
+2> Raspberry Pi Power Supply
+3> Micro-SD Card
+4> Card Reader and Computer
+"""
+
+#DetailedListOfIngredients
+"""
+1> Raspberry Pi SBC with Wifi.
+> Cheapest: Raspberry Pi Zero W or WH.
+> Recommended: Raspberry Pi Zero 2 W or WH.
+> Ultimate: Raspberry Pi 3, 4, 5 or latest.
+2> Raspberry Pi Power Supply.
+> Will do: 5V Micro-USB Charger for Raspberry Pi Zero W or WH.
+> Ultimate: Official Power Supply.
+3> Micro-SD Card.
+> Will do: 8Gb to 32Gb Class 10 or A1.
+> Ultimate: Expensive cards that one can afford. Tehe.
+4> Card Reader and Computer.
+> Will do: Generic Card Readers.
+> Recommended: Not-so-cheap Card Readers that one trusts.
+> Ultimate: Expensive card readers that one can flex. Tehe.
+> One is supposed to have a computer to cook some Raspberry Pi.
+"""
